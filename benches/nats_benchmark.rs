8d77fee7b56466251256ad8ce9372ca0949c6844
Pete LeVasseur <plevasseur@gmail.com>
2024-09-29 01:02:38 -0400

    Move common definitions into the library crate and out of benchmarks

--- benches/nats_benchmark.rs
+++ benches/nats_benchmark.rs
@@ -2,39 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use prost::Message;
 use std::time::Duration;
 use tokio::runtime::Runtime;
-
-const NATS_URL: &str = "nats://127.0.0.1:4222";
-const NUM_MESSAGES: u64 = 1000;
-const INPUT: &str = "Input";
-
-#[derive(prost::Message)]
-#[must_use]
-struct TestMessage {
-    #[prost(string, tag = "1")]
-    id: String,
-    #[prost(string, tag = "2")]
-    request_id: String,
-    #[prost(string, tag = "3")]
-    correlation_id: String,
-    #[prost(string, tag = "4")]
-    source_id: String,
-    #[prost(string, tag = "5")]
-    target_id: String,
-    #[prost(bytes = "vec", tag = "6")]
-    content: Vec<u8>,
-}
-
-// Create a test message for publishing/requesting
-fn test_message(source_id: String) -> TestMessage {
-    TestMessage {
-        id: "73aea97e-af46-4e54-bae4-c33a10466f99".into(),
-        request_id: "bc5272c9-37ed-4ba9-afcc-5c18ed8abe31".into(),
-        correlation_id: "a09bdd8c-18d9-4c4a-80ca-707f225e4ce3".into(),
-        source_id,
-        target_id: "test".into(),
-        content: vec![0; 182],
-    }
-}
+use zenoh_benchmark::{test_message, NATS_URL, NUM_MESSAGES, DURATION, INPUT};
 
 async fn nats_pubsub(client: async_nats::Client, num_messages: u64) {
     for _ in 0..num_messages {
@@ -54,7 +22,7 @@ fn pubsub_benchmark(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("Pub-Sub");
     group.throughput(Throughput::Elements(NUM_MESSAGES));
-    group.measurement_time(Duration::from_secs(30));
+    group.measurement_time(Duration::from_secs(DURATION));
 
     let connect_opts = async_nats::ConnectOptions::new()
         .retry_on_initial_connect()
--- benches/zenoh_benchmark.rs
+++ benches/zenoh_benchmark.rs
@@ -4,39 +4,7 @@ use prost::Message;
 use tokio::runtime::Runtime;
 use zenoh::{self, config::Config, Session, Wait};
 use log::*;
-
-const NUM_MESSAGES: u64 = 1000;
-const DURATION: u64 = 30;
-const INPUT: &str = "Input";
-
-#[derive(Message)]
-#[must_use]
-struct TestMessage {
-    #[prost(string, tag = "1")]
-    id: String,
-    #[prost(string, tag = "2")]
-    request_id: String,
-    #[prost(string, tag = "3")]
-    correlation_id: String,
-    #[prost(string, tag = "4")]
-    source_id: String,
-    #[prost(string, tag = "5")]
-    target_id: String,
-    #[prost(bytes = "vec", tag = "6")]
-    content: Vec<u8>,
-}
-
-// Create a test message for publishing/requesting
-fn test_message(source_id: String) -> TestMessage {
-    TestMessage {
-        id: "73aea97e-af46-4e54-bae4-c33a10466f99".into(),
-        request_id: "bc5272c9-37ed-4ba9-afcc-5c18ed8abe31".into(),
-        correlation_id: "a09bdd8c-18d9-4c4a-80ca-707f225e4ce3".into(),
-        source_id,
-        target_id: "test".into(),
-        content: vec![0; 182],
-    }
-}
+use zenoh_benchmark::{test_message, DURATION, NUM_MESSAGES, INPUT};
 
 async fn start_sub() {
     let session = zenoh::open(Config::default())
@@ -82,19 +50,13 @@ pub fn pubsub_benchmark(c: &mut Criterion) {
     group.throughput(Throughput::Elements(NUM_MESSAGES));
     group.measurement_time(Duration::from_secs(DURATION));
 
-    // TODO: Figure out why opening and closing the Zenoh Session
-    // within the benchmark_function causes this to malfunction
     let session = zenoh::open(zenoh::Config::default())
         .wait()
         .expect("Unable to start publisher session");
     group.bench_function("zenoh", |b| {
         b.to_async(&runtime)
             .iter(|| async { 
-                // let session = zenoh::open(zenoh::Config::default())
-                //     .await
-                //     .expect("Unable to start publisher session");
                 send_pub(session.clone(), NUM_MESSAGES).await;
-                // session.close().await.expect("Unable to close sesion");
             });
     });
 
